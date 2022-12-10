23c9e8ae639109efc30a3e86907760d72cfca43f
Admin <iggla67@yandex.ru>
2022-12-10 06:28:39 +0400

    5.

---
+++ src/Transport/Bus.java
@@ -0,0 +1,8 @@
+package Transport;
+
+public class Bus extends Auto {
+    public Bus(String brand, String model, int year, String country) {
+        super(brand, model, year, country);
+    }
+
+}
